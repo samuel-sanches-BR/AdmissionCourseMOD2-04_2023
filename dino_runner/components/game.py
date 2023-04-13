9a43522a9048012d46c3ff1a8ab5a31ef69b1adf
Samuel Sanches <samuelsanches@gmail.com>
2023-04-13 16:31:01 -0300

    duck

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,6 +1,7 @@
 import pygame
 
 from dino_runner.components.dinosaur import Dinosaur
+
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 
 
@@ -34,7 +35,6 @@ class Game:
     def update(self):
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
-        
 
     def draw(self):
         self.clock.tick(FPS)
