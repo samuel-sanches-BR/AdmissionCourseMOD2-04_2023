78b4a13263221eea538e3bb489551a2d1883faf5
Samuel Sanches <samuelsanches@gmail.com>
2023-04-14 15:24:21 -0300

    Cactus Pequeno

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -2,6 +2,8 @@ import pygame
 
 from dino_runner.components.dinosaur import Dinosaur
 
+from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
+
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 
 
@@ -17,6 +19,7 @@ class Game:
         self.x_pos_bg = 0
         self.y_pos_bg = 380
         self.player = Dinosaur()
+        self.obstacle_manager = ObstacleManager()
         
     def run(self):
         # Game loop: events - update - draw
@@ -33,6 +36,7 @@ class Game:
                 self.playing = False
 
     def update(self):
+        self.obstacle_manager.update(self)
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
 
@@ -41,6 +45,7 @@ class Game:
         self.screen.fill((255, 255, 255)) #Também aceita código hexadecimal "#FFFFFF"
         self.draw_background()
         self.player.draw(self.screen)
+        self.obstacle_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
